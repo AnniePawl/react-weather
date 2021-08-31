122bb7be0947c8998c0efdd9e738e438df18e8e3
Anna Pawl <annampawl@gmail.com>
2021-08-31 19:44:57 -0400

    installed and implemented axios

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,46 +1,50 @@
-import React from 'react';
-import {useState} from 'react' //react hook
+import React, { useState } from "react";
+import axios from "axios";
 
-function Form() {
-  const [zip, setZip] = useState('')
-  const [data, setData] = useState('')
+export default function Form() {
+  const [zip, setZip] = useState("");
+  const [data, setData] = useState(null);
+  const apikey = "0853c15130697cb0030dc6c83d7ae9cd";
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=imperial`;
 
-  // get weather data from api 
-  async function getWeather(){
-    try{
-      const apikey = '0853c15130697cb0030dc6c83d7ae9cd'
-      const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=imperial`
-      const res = await fetch(path)
-      const json = await res.json()
-      setData(json)
-      console.log(json)
-    }
-    catch(err){
-      console.log(err.message)
+  const getStories = () => {
+    axios.get(path).then((res) => {
+      const data = res.data;
+      setData(data);
+      console.log(data);
+    });
+  };
 
-    }
-  }
-
-  console.log(zip)
-    return (
-      <div>
-        <div>
-          {data ? <h1>{data.main.temp}</h1>: null}
-        </div>
-      <form onSubmit={(e)=>{
-        e.preventDefault() //prevents page refresh
-        getWeather() //get data from api
-      }}>
+  return (
+    <div>
+      {data ? <h1>{data.main.temp}</h1> : null}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); //prevent page reload
+          getStories();
+        }}
+      >
         <input
-        type='text'
-        placeholder='Enter ZIP'
-        value = {zip}
-        onChange = {(e)=>{setZip(e.target.value)}}
+          type="text"
+          placeholder="Enter ZIP"
+          value={zip}
+          onChange={(e) => {
+            setZip(e.target.value);
+          }}
         />
-        <button type='submit'>Enter</button>
+        <button type="submit">Enter</button>
       </form>
-      </div>
-      );
+    </div>
+  );
 }
- 
-export default Form;
+
+// async function getWeather() {
+//   try {
+//     const res = await fetch(path);
+//     const json = await res.json();
+//     setData(json);
+//     console.log(json);
+//   } catch (err) {
+//     console.log(err.messege);
+//   }
+// }
